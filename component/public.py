0b90ecbddefa62f759c2dba55ca1103b1bab15c6
= <3590585549@qq.com>
2025-07-27 21:32:58 +0800

    bug已更改

--- component/public.py
+++ component/public.py
@@ -2,8 +2,8 @@ def hello():
     print('Hello-模块-public')
 
 
-def message(mes,num = 3):
-    if num > 2:
-        return
+def message(mes: str, num: int = 1) -> None:
+    if num <= 2:
+        print(f'你好{mes}')
 
-    print(f'你好{mes}')
+
