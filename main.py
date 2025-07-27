09376371afb7caa62d48ba810348e4809255ce69
xiaoyu00615 <daizy2005@qq.com>
2025-07-27 17:11:01 +0800

    人脸识别分支

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # 这是一个示例 Python 脚本。
 
-import  component.public as public
+import component.public as public
+import component.face as face
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
@@ -21,6 +22,7 @@ def main():
     public.Hellow()
 
 main()
+face.test()
 
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
